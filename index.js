ef00e0174737c9271922a228bf9c189efe944d4a
hpham474 <hpham474@gmail.com>
2024-10-23 04:05:42 -0500

    rewrite site using express

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-var http = require("http");
-var url = require("url");
-var fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-http
-  .createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    if (q.pathname === "/") {
-      var filename = "./index.html";
-    } else {
-      var filename = "." + q.pathname + ".html";
-    }
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8080);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+// Handle 404
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
